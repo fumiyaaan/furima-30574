3f4d536a525cef1656f932f7200124a4ec576e0b
fumiyaaan <indestructible.peace@gmail.com>
2020-10-15 22:10:11 +0900

    正規表現を変数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,14 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /[a-z]\d/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  with_options presence: true, format: { with: NAME_REGEX, message: 'Full-width characters' } do
     validates :sur_name
     validates :first_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
+  NAME_READING_REGEX = /\A[ァ-ン]+\z/.freeze
+  with_options presence: true, format: { with: NAME_READING_REGEX, message: 'Full-width katakana characters' } do
     validates :sur_name_reading
     validates :first_name_reading
   end
