0e0911ca8e87d166cc81a8b2bde9846ffdc202a0
fumiyaaan <indestructible.peace@gmail.com>
2020-10-16 19:19:23 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,26 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Association::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :transport_day
+
+  validates :image, presence: true #検討中
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :condition, presence: true
+  validates :postage_payer, presence: true
+  validates :prefecture, presence: true
+  validates :transport_day, presence: true
+
+  #ジャンルの選択が「---」の時は保存できないバリデーション
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :postage_payer_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 0 }
+  validates :transport_day_id, numericality: { other_than: 1 }
+
+  belongs_to :user
 end
