d85702c2f7637f2205982fcd71c069c3ef6458c6
fumiyaaan <indestructible.peace@gmail.com>
2020-10-19 16:32:26 +0900

    手数料・利益の表示機能実装

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,3 +1,12 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-})
+function price () {
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const Profit = document.getElementById("profit");
+  priceInput.addEventListener('input', () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
+    Profit.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
+  })
+}
+window.addEventListener("load", price);
