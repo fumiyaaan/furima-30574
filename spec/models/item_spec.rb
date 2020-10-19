48ee0cb70af4c16aec884f823ccddbd3dc048e65
fumiyaaan <indestructible.peace@gmail.com>
2020-10-19 20:22:45 +0900

    モデル単体テストコード　やり直し

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,39 +7,26 @@ RSpec.describe Item, type: :model do
 
   describe '商品出品' do
     context '商品出品がうまくいくとき' do
-      it 'image, name, description, category_id, condition_id, postage_payer, prefecture_id, transport_day, priceが存在すれば出品できる' do
-        @item.category_id = 2
-        @item.condition_id = 2
-        @item.postage_payer_id = 2
-        @item.prefecture_id = 1
-        @item.transport_day_id = 2
-        @item.price = 1000
+      it 'image, name, description, category_id, condition_id, postage_payer_id, prefecture_id, transport_day_id, priceが存在すれば出品できる' do
         expect(@item).to be_valid
       end
 
       it 'priceが300~9,999,999円の間であれば出品できる' do
-        @item.category_id = 2
-        @item.condition_id = 2
-        @item.postage_payer_id = 2
-        @item.prefecture_id = 1
-        @item.transport_day_id = 2
         @item.price = 300
         expect(@item).to be_valid
       end
 
       it 'priceが半角数字での入力であれば出品できる' do
-        @item.category_id = 2
-        @item.condition_id = 2
-        @item.postage_payer_id = 2
-        @item.prefecture_id = 1
-        @item.transport_day_id = 2
-        @item.price = 50_000
+        @item.price = 50000
         expect(@item).to be_valid
       end
     end
 
     context '商品出品がうまくいかないとき' do
       it 'imageが空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it 'nameが空だと出品できない' do
         @item.name = ''
@@ -52,6 +39,37 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
+
+      it 'category_idが1である場合出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category Select")
+      end
+
+      it 'condition_idが1である場合出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition Select")
+      end
+
+      it 'postage_payer_idが1である場合出品できない' do
+        @item.postage_payer_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage payer Select")
+      end
+
+      it 'prefecture_idが0である場合出品できない' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture Select")
+      end
+
+      it 'transport_day_idが1である場合出品できない' do
+        @item.transport_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Transport day Select")
+      end
+
       it 'priceが空だと出品できない' do
         @item.price = ''
         @item.valid?
