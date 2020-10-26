419d7bf64c3aee101c9096d00b0d70abcaefb900
fumiyaaan <indestructible.peace@gmail.com>
2020-10-26 12:41:47 +0900

    フォーム情報の保存機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,24 @@
 class PurchasesController < ApplicationController
   def index
-    @purchase = PurchaseAddress.new
+    @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(set_params)
+    
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+  
+  def set_params
+    params.require(:purchase_address).permit(:postal_number, :prefecture_id, :city, :house_number, :building_name, :phone_number, :purchase_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,18 +1,22 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_number, :prefecture, :city, :house_number, :building_name, :phone_number
-
+  attr_accessor :postal_number, :prefecture_id, :city, :house_number, :building_name, :phone_number, :purchase_id, :user_id, :item_id
+  
  
-  POSTAL_NUMBER_REGEX = /\A\d{3}[-]\d{4}\z/.freeze
-  validates :postal_number, presence: true, with: POSTAL_NUMBER_REGEX
+  POSTAL_NUMBER_REGEX = /\d{3}[-]\d{4}/.freeze              
+  validates :postal_number, presence: true, format: { with: POSTAL_NUMBER_REGEX, message: 'input correctly' }
   validates :prefecture_id, presence: true, numericality: { other_than: 0, message: 'Select' }
   validates :city, presence: true
   validates :house_number, presence: true
-  PHONE_NUMBER_REGEX = [0-9]{,11}
-  validates :phone_number, presence: true, with: PHONE_NUMBER_REGEX
 
 
+  PHONE_NUMBER_REGEX = /\A[0-9]{,11}\z/.freeze
+  validates :phone_number, presence: true
+  validates :phone_number, numericality: { with: PHONE_NUMBER_REGEX, message: 'Input only number' }
+  #validates :price, numericality: { message: 'Half-width number' }
+  
   def save
-    Adress.create(postal_number: postal_number, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_number: postal_number, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
