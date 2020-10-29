dc981d32a0a8c70a6c62b7b3e9ff3e0dea85e48d
fumiyaaan <indestructible.peace@gmail.com>
2020-10-29 12:42:11 +0900

    itemsコントローラーupdateアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,11 +30,10 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if user_signed_in? && current_user.id == @item.user_id
-      @item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
-      redirect_to root_path
+      render :edit
     end
   end
 
